dc5953e648b2945098c985baafbae1f542a3888e
Archimedes Trajano <developer@trajano.net>
2019-03-10 16:05:27 -0400

    Implemented docker detection

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This provide:
 * a `DiscoveryClient` that works with Docker Swarm Services
-* an `ApplicationEnvironmentPreparedEvent` listener that would enable `docker` profile if it detects that the container is running in a Docker environment
+* an `ApplicationContextInitializer` that would enable `docker` profile if it detects that the container is running in a Docker environment
--- src/main/java/net/trajano/cloud/swarm/profile/DockerSwarmApplicationContextInitializer.java
+++ src/main/java/net/trajano/cloud/swarm/profile/DockerSwarmApplicationContextInitializer.java
@@ -1,7 +1,10 @@
 package net.trajano.cloud.swarm.profile;
 
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.context.ApplicationListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,11 +13,6 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-
 /**
  * Enable the {@code docker} profile if it detects it is running inside a Docker
  * environment.
@@ -22,17 +20,8 @@ import org.springframework.core.annotation.Order;
  * @see <a href="https://stackoverflow.com/a/20012536/242042">How to determine
  *      if a process runs inside lxc/Docker?</a>
  */
-@Order(Ordered.HIGHEST_PRECEDENCE + 1)
-public class DockerProfileApplicationListener implements
-    ApplicationListener<ApplicationEnvironmentPreparedEvent> {
-
-    private static final Log LOG = LogFactory
-        .getLog(DockerProfileApplicationListener.class);
-
-    /**
-     * Profile name.
-     */
-    private static final String PROFILE_NAME = "docker";
+public class DockerSwarmApplicationContextInitializer implements
+    ApplicationContextInitializer {
 
     /**
      * Pattern to locate in {@code /proc/1/cgroup} to determine if the application
@@ -40,14 +29,27 @@ public class DockerProfileApplicationListener implements
      */
     private static final Pattern DOCKER_CHECK_RE = Pattern.compile("^1:[^:]+:/docker/[0-9a-f]+");
 
-    @Override
-    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+    /**
+     * Logger.
+     */
+    private static final Log LOG = LogFactory
+        .getLog(DockerSwarmApplicationContextInitializer.class);
 
-        System.out.println("HERE" + Arrays.asList(event.getEnvironment().getActiveProfiles()));
-        if (!Arrays.asList(event.getEnvironment().getActiveProfiles()).contains(PROFILE_NAME) && isRunningInDockerContainer()) {
+    /**
+     * Profile name.
+     */
+    private static final String PROFILE_NAME = "docker";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+
+        final ConfigurableEnvironment environment = configurableApplicationContext.getEnvironment();
+        if (!Arrays.asList(environment.getActiveProfiles()).contains(PROFILE_NAME) && isRunningInDockerContainer()) {
             LOG.debug("Adding 'docker' profile.");
-            event.getEnvironment().addActiveProfile(PROFILE_NAME);
-            event.getEnvironment().addActiveProfile("foo");
+            environment.addActiveProfile(PROFILE_NAME);
         }
     }
 
